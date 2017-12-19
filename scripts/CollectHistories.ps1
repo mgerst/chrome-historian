182b1423d7b73f9006ed789ea03c2ed43f211b93
Matt Gerst <mattgerst@gmail.com>
2017-12-19 17:56:01 -0600

    Powershell collect script now takes arguments

--- scripts/CollectHistories.ps1
+++ scripts/CollectHistories.ps1
@@ -1,18 +1,27 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$userListPath = C:\Users\$env:USERNAME\users.csv,
+
+    [Parameter(Mandatory=$True)]
+    [string]$historyPath = C:\Users\$env:USERNAME\histories
+)
+
 Write-Host -ForegroundColor Yellow "############################################"
 Write-Host -ForegroundColor Green "Collect chrome histories in a chrome-historian ready format"
 
 Write-Host -ForegroundColor Yellow "############################################"
 Write-Host -ForegroundColor Green "Part 1: Get list of users"
-Write-Host -ForegroundColor Yellow "Exporting the list of users to C:\users\%username%\users.csv"
+Write-Host -ForegroundColor Yellow "Exporting the list of users to $userListPath"
 
 # List the users in C:\Users and export to the local profile for calling later
-dir C:\Users | select Name | Export-Csv -Path C:\Users\$env:USERNAME\users.csv -NoTypeInformation
-$list = Test-Path C:\Users\$env:USERNAME\users.csv
+dir C:\Users | select Name | Export-Csv -Path $userListPath -NoTypeInformation
+$list = Test-Path $userListPath
 
 Write-Host -ForegroundColor Yellow "############################################"
 Write-Host -ForegroundColor Green "Part 2: Grabbing Histories"
-Write-Host -ForegroundColor Yellow "Writing histories to C:\Users\%username%\histories"
-$histdb = C:\Users\$env:USERNAME\histories
+Write-Host -ForegroundColor Yellow "Writing histories to $historyPath"
+$histdb = $historyPath
 New-Item $histdb -type directory
 
 if ($list) {
